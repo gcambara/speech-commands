c34fbe04b525c3bc731eac2646ce7ec6b1f8c668
Guillermo Cámbara Ruiz <guillermocambara@gmail.com>
2022-05-04 21:08:46 +0200

    Added several w2v2 training recipes

---
+++ recipes/perceiver/wav2vec2_latents/train_w2v2_160.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CFG=recipes/perceiver/wav2vec2_latents/train.yaml
+GPU=3
+SAVE_DIR=recipes/perceiver/wav2vec2_latents/results/train_w2v2_160_latents_cluster
+export CUDA_VISIBLE_DEVICES=$GPU
+cd ../../../
+
+NUM_LATENTS=160
+
+BATCH_SIZE=32
+CLASSIFIER=perceiver_w2v2
+CLUSTERIZE=1
+EPOCHS=400
+LR=0.0001
+LR_GAMMA=0.98
+OPTIMIZER=adamw
+SEED=0
+
+echo "Running Perceiver W2V2 experiment with $NUM_LATENTS latents..."
+python train.py --config $CFG --batch_size $BATCH_SIZE --batch_size_dev $BATCH_SIZE --batch_size_test $BATCH_SIZE --lr $LR --lr_gamma $LR_GAMMA --optimizer $OPTIMIZER --run_dir $SAVE_DIR --classifier $CLASSIFIER --max_epochs $EPOCHS --prc_num_latents $NUM_LATENTS --clusterize_latents $CLUSTERIZE --seed $SEED
