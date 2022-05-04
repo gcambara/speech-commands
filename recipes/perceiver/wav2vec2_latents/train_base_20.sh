e5951664c8759e10e4e5b3c9d81447999591a4c1
Guillermo Cámbara Ruiz <guillermocambara@gmail.com>
2022-05-04 21:12:30 +0200

    Added train BASE recipes

---
+++ recipes/perceiver/wav2vec2_latents/train_base_20.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CFG=recipes/perceiver/wav2vec2_latents/train.yaml
+GPU=3
+SAVE_DIR=recipes/perceiver/wav2vec2_latents/results/train_base_20
+export CUDA_VISIBLE_DEVICES=$GPU
+cd ../../../
+
+NUM_LATENTS=20
+
+BATCH_SIZE=32
+CLASSIFIER=perceiver
+CLUSTERIZE=0
+EPOCHS=400
+LR=0.0001
+LR_GAMMA=0.98
+OPTIMIZER=adamw
+SEED=0
+
+echo "Running Perceiver BASE experiment with $NUM_LATENTS latents..."
+python train.py --config $CFG --batch_size $BATCH_SIZE --batch_size_dev $BATCH_SIZE --batch_size_test $BATCH_SIZE --lr $LR --lr_gamma $LR_GAMMA --optimizer $OPTIMIZER --run_dir $SAVE_DIR --classifier $CLASSIFIER --max_epochs $EPOCHS --prc_num_latents $NUM_LATENTS --clusterize_latents $CLUSTERIZE --seed $SEED
