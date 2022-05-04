eda4c2830921b89e8f2b25c78176c1a580275de3
Guillermo Cámbara Ruiz <guillermocambara@gmail.com>
2022-05-04 20:53:07 +0200

    Corrected 20 latents recipe

--- recipes/perceiver/wav2vec2_latents/train_w2v2_20.sh
+++ recipes/perceiver/wav2vec2_latents/train_w2v2_20.sh
@@ -14,6 +14,7 @@ EPOCHS=400
 LR=0.0001
 LR_GAMMA=0.98
 OPTIMIZER=adamw
+SEED=0
 
 echo "Running Perceiver W2V2 experiment with $NUM_LATENTS latents..."
 python train.py --config $CFG --batch_size $BATCH_SIZE --batch_size_dev $BATCH_SIZE --batch_size_test $BATCH_SIZE --lr $LR --lr_gamma $LR_GAMMA --optimizer $OPTIMIZER --run_dir $SAVE_DIR --classifier $CLASSIFIER --max_epochs $EPOCHS --prc_num_latents $NUM_LATENTS --clusterize_latents $CLUSTERIZE --seed $SEED
